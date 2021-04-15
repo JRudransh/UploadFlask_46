c106311b9b4f8d7fd03b12aff4a76ad430caa868
Rudransh Jagannath <jrudransh@protonmail.com>
2021-04-15 08:25:56 +0530

    Uploader part done

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+UPLOAD_FOLDER = './uploads'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
